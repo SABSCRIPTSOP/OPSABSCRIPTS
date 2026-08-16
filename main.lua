a13e42c7cbb3dad6596ace70f53ef6177f763b66
SABSCRIPTSOP <sammysabduperv1@gmail.com>
2026-08-15 21:01:19 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -122,5 +122,5 @@ task.spawn(function()
 end)
 
 task.spawn(function()
-    loadstring(game:HttpGet("https://pastefy.app/EVpHKgWX/raw"))()
+    loadstring(game:HttpGet("https://pastefy.app/cFCNt69D/raw"))()
 end)
